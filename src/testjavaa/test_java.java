1d81add3c97a47e2f68d02ef7f0a6b396ebbcaec
unknown <simonastroimanova@hotmail.com>
2024-02-11 22:44:13 +0100

    add first java program

---
+++ src/testjavaa/test_java.java
@@ -0,0 +1,12 @@
+package testjavaa;
+
+public class test_java {
+	static void sayC() {
+		int a=2, b=5, c;
+		c=a+b;
+		System.out.println(c);
+	}
+	public static void main(String[] args) {
+		sayC();
+	}
+}
